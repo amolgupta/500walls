e2ffa5ed98c3a6ebface5a616f7a6f8e81df74ed
Akshay S Dinesh <asdofindia@gmail.com>
2014-10-18 09:07:06 +0530

    universalised, and easy installation

--- downlod.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd /home/amol/Desktop/500px/
-wget -q -O- --header="Accept: text/html"  -U 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:13.0) Gecko/20100101 Firefox/13.0.1' http://feed.500px.com/500px-best | grep -oP 'http://ppcdn.500px.org.+.jpg'  | sed  -e "s/4.jpg/5.jpg/g" | while read line ; do wget "$line"  ; done
-
-
-
